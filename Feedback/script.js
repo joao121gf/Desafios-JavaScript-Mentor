49ef1a3e832d22b136aed64372f14b57c780d94d
João Giovani <giovannirs957@gmail.com>
2023-06-18 16:15:33 -0300

    Projeto finalizado

--- Feedback/script.js
+++ Feedback/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
-  const numeros = document.querySelectorAll("span");
+  const numeros = document.querySelectorAll(".numeros-box span");
 
   function ativaLaranja() {
     if (this.classList.contains("ativoLaranja")) {
@@ -18,21 +18,40 @@ window.addEventListener("load", () => {
 
   const button = document.querySelector(".button");
   const containerTy = document.querySelector(".ty");
-  const containerBox = document.querySelector('.container-box')
-  const load = document.querySelector('.load')
+  const containerBox = document.querySelector(".container-box");
+  const load = document.querySelector(".load");
+  const number = document.querySelector("[data-number]");
+  const select = document.querySelector('.correct')
+
 
 
   function openTy() {
-    load.style.opacity = '1'
-    setTimeout(()=> {
-      containerTy.style.opacity = "1";
-      containerTy.style.zIndex = '1'
-      containerTy.classList.add('active')
-      containerBox.style.opacity = '0'
-    }, 1500)}
+    const array = Object.values(numeros)
+
+     const todosComAtivo = array.every((n) =>{
+       return !n.classList.contains('ativoLaranja')
+     }) 
+     if (todosComAtivo){
+       select.style.opacity = '1'
+     }
 
 
+    numeros.forEach((n, index) => {
+      if (n.classList.contains("ativoLaranja")) {
+        select.style.opacity = '0'
+
+        number.innerText = n.innerText;
+
+        load.style.opacity = "1";
+        setTimeout(() => {
+          containerTy.style.opacity = "1";
+          containerTy.style.zIndex = "1";
+          containerTy.classList.add("active");
+          containerBox.style.opacity = "0";
+        }, 1500);
+      }
+    });
+  }
 
   button.addEventListener("click", openTy);
-  console.log(button)
 });
